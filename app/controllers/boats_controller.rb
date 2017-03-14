ba4d0eb5e3adbe57cffda72dcbd3ab2f6b224299
Guillaume Salin <gsalin@gmail.com>
2017-03-14 19:04:48 +0100

    facebook connect ok, skip before action for home and show ok, css for cards little better

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,5 +1,5 @@
 class BoatsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_boat, only: [:show, :edit, :update, :destroy]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'boats#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
