6d185febdfaad8ad1a0e3d108d5687a63014fb67
Edouard Castaignet <ecastaignet@gmail.com>
2017-03-18 12:55:20 +0100

    some stuff done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -9,6 +9,4 @@ class Boat < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :description, presence: true
-  validates :price_per_day, presence: true
-  validates :nb_of_passengers, presence: true
 end
